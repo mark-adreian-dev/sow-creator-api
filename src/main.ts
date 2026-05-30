e6b346de334fa442b3ff86f8fe7df826092cb9d1
mark-adreian-dev <markadreianramos@gmail.com>
2026-05-30 13:52:21 +0800

    configuring docker

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,12 @@ async function bootstrap() {
     }),
   );
 
-  const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173'];
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'http://localhost:4173',
+    'http://127.0.0.1:4173',
+  ];
 
   app.enableCors({
     origin: allowedOrigins,
